d9b617f05b4e0b77aa9c80ad737bbd09eba0aab7
CoderDongHuang <1218798773@qq.com>
2026-06-30 21:32:43 +0800

    评价反馈的数据层

---
+++ campus-review/src/main/java/com/sharecampus/review/mapper/OrderReviewMapper.java
@@ -0,0 +1,6 @@
+package com.sharecampus.review.mapper;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.sharecampus.review.entity.OrderReview;
+import org.apache.ibatis.annotations.Mapper;
+@Mapper
+public interface OrderReviewMapper extends BaseMapper<OrderReview> {}
