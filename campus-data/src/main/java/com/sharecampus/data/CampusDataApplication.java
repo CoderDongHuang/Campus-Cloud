48713346d14627b72a0fbd909a5842902b4ae5ae
CoderDongHuang <1218798773@qq.com>
2026-07-01 13:32:42 +0800

    数据分析服务- 配置、依赖、启动类

---
+++ campus-data/src/main/java/com/sharecampus/data/CampusDataApplication.java
@@ -0,0 +1,6 @@
+package com.sharecampus.data;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+@SpringBootApplication @EnableDiscoveryClient
+public class CampusDataApplication { public static void main(String[] args) { SpringApplication.run(CampusDataApplication.class, args); } }
