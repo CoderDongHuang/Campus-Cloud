2afc053597405fa68edd38df6976082cb7955cbc
CoderDongHuang <1218798773@qq.com>
2026-07-07 15:54:09 +0800

    IM消息加sessionId和senderId复合索引
    ImMessage: 加@CompoundIndex idx_session_time(sessionId+createdAt)和idx_sender_time(senderId+createdAt)
    
    解决消息历史查询和按发送者查询的全表扫描问题

--- campus-im/src/main/java/com/sharecampus/im/entity/ImMessage.java
+++ campus-im/src/main/java/com/sharecampus/im/entity/ImMessage.java
@@ -2,12 +2,18 @@ package com.sharecampus.im.entity;
 
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
+import org.springframework.data.mongodb.core.index.CompoundIndexes;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Date;
 
 @Data
 @Document(collection = "im_message")
+@CompoundIndexes({
+    @CompoundIndex(name = "idx_session_time", def = "{'sessionId': 1, 'createdAt': -1}"),
+    @CompoundIndex(name = "idx_sender_time", def = "{'senderId': 1, 'createdAt': -1}")
+})
 public class ImMessage {
     @Id
     private String id;
